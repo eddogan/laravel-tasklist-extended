b084608970cc123dd23461a0d28c925710cba8f1
Ed Dogan <ed@adogandesign.com>
2018-02-08 00:45:18 -0600

    Add files via upload

---
+++ database/migrations/2016_02_04_041533_create_tasks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->index();
+            $table->string('name');
+            $table->string('duedate');
+            $table->string('priority');
+            $table->string('project');
+            $table->string('assignedto');
+            $table->string('assignedby');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tasks');
+    }
+}
